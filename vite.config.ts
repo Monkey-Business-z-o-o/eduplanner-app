77206b2e897b433cd19bcbe23bc4a3792954eec1
aleksander <abastek@kanga.exchange>
2025-01-06 13:11:58 +0100

    Add logout and fill proxy with new paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,57 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const eduplannetServerUrl = 'http://localhost:8080'
+
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		port: 3000, // Frontend na porcie 3000
 		proxy: {
 			'/demo-data': {
-				target: 'http://localhost:8080', // Backend na porcie 8080
+				target: eduplannetServerUrl,
 				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/demo-data/, '/demo-data') // Zostawiamy ścieżkę bez zmian
+				rewrite: (path) => path.replace(/^\/demo-data/, '/demo-data')
 			},
 			'/timetables': {
-				target: 'http://localhost:8080', // Backend na porcie 8080
+				target: eduplannetServerUrl,
 				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/timetables/, '/timetables') // Zostawiamy ścieżkę bez zmian
+				rewrite: (path) => path.replace(/^\/timetables/, '/timetables')
+			},
+			'/account/login': {
+				target: eduplannetServerUrl,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/account\/login/, '/account/login')
+			},
+			'/account/signup': {
+				target: eduplannetServerUrl,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/account\/signup/, '/account/signup')
+			},
+			'/account/logout': {
+				target: eduplannetServerUrl,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/account\/logout/, '/account/logout')
+			},
+			'/account/login?error': {
+				target: eduplannetServerUrl,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/account\/login\?error/, '/account/login?error')
+			},
+			'^/css/.*': {
+				target: eduplannetServerUrl,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/css\//, '/css/')
+			},
+			'^/js/.*': {
+				target: eduplannetServerUrl,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/js\//, '/js/')
+			},
+			'^/img/.*': {
+				target: eduplannetServerUrl,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/img\//, '/img/')
 			}
 		}
 	}
