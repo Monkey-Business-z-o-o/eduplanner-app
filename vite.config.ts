b439e85e477ca85c6a0915679a19676ee78850f8
s27281-pj <115408624+s27281-pj@users.noreply.github.com>
2025-01-12 13:01:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-
-// Ładowanie zmiennych środowiskowych
-dotenv.config();
-
-// Pobranie zmiennej API_URL z pliku .env lub ustawienie domyślnej wartości
-const API_URL = process.env.VITE_API_URL || 'http://localhost:8081';
 
 export default defineConfig({
   plugins: [sveltekit()],
   build: {
-    outDir: 'dist',  // Folder do którego zostanie zbudowany frontend
-    emptyOutDir: true,
+    outDir: 'dist', // Folder, do którego zostanie zbudowany frontend
+    emptyOutDir: true, // Czy wyczyścić folder outDir przed zbudowaniem
   },
   server: {
-    port: 3000,
+    host: '0.0.0.0', // Nasłuchiwanie na wszystkich interfejsach sieciowych
+    port: 3000, // Port, na którym będzie uruchomiony frontend
     proxy: {
+      // Przekierowanie wszystkich endpointów na backend
       '^/(timetables|lessons|demo-data|timeslots|rooms|profile).*': {
-        target: API_URL,  // Użycie wartości z env
+        target: 'http://localhost:8081',
         changeOrigin: true,
-        rewrite: (path) => path, // Dodatkowe ustawienie przekierowania ścieżek, jeśli potrzeba
+        rewrite: (path) => path,
       },
     },
   },
