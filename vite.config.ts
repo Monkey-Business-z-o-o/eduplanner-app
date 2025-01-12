e21199fedcbb55f85cacf161031f7b368cd577a1
s27281-pj <115408624+s27281-pj@users.noreply.github.com>
2025-01-12 13:01:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit()],
   build: {
-    outDir: 'dist', // Folder do którego zostanie zbudowany frontend
+    outDir: 'dist', // Folder, do którego zostanie zbudowany frontend
     emptyOutDir: true, // Czy wyczyścić folder outDir przed zbudowaniem
   },
   server: {
-    port: 3000, // Port na którym będzie uruchomiony frontend
+    host: '0.0.0.0', // Nasłuchiwanie na wszystkich interfejsach sieciowych
+    port: 3000, // Port, na którym będzie uruchomiony frontend
     proxy: {
       // Przekierowanie wszystkich endpointów na backend
       '^/(timetables|lessons|demo-data|timeslots|rooms|profile).*': {
