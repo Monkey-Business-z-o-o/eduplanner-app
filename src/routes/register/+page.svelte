a193ddc62b2454660ea51704f0c189e03a8aff23
aleksander <abastek@kanga.exchange>
2025-01-20 14:11:26 +0100

    edit labels

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -19,15 +19,15 @@
                 </h1>
                 <form class="space-y-4 md:space-y-6" method="POST" action="?/login">
                     <div>
-                        <label for="First name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <label for="First name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
                         <input type="text" name="firstName" id="firstName" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Jozin" required>
                     </div>
                     <div>
-                        <label for="Last name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <label for="Last name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
                         <input type="text" name="lastName" id="lastName" placeholder="Zbazin" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                     </div>
                     <div>
-                        <label for="login" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <label for="login" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Login</label>
                         <input type="text" name="login" id="login" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
                     </div>
                     <div>
